41131c1b893a2b2bd0febae6c72ea8961272c2c2
janesteban <janestebansan@outlook.com>
2020-10-21 17:39:25 +0200

    FEAT: Add LamportMessage class.

--- src/Lamport/LamportMessage.java
+++ src/Lamport/LamportMessage.java
@@ -1,4 +1,18 @@
 package Lamport;
 
-public class LamportMessage {
+import java.io.Serializable;
+
+public class LamportMessage implements Serializable {
+
+    private final int clock;
+    private final int id;
+
+
+    public LamportMessage(int clock, int id){
+
+        this.clock = clock;
+        this.id = id;
+        
+    }
+
 }
